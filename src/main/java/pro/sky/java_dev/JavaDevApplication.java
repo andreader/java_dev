b39634a5ce5f648f8978a563231eb549567e51f8
Андрей <79538233369@ya.ru>
2024-07-04 18:47:27 +0500

    initial commit

---
+++ src/main/java/pro/sky/java_dev/JavaDevApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevApplication.class, args);
+    }
+
+}
